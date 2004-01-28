7ca844639964e736d61076cc55857ef1386d910d
jnegre <jnegre>
2004-01-28 18:17:43 +0000

    Button to call the search dialog

--- org.jnegre.allthenews/src/org/jnegre/allthenews/pref/SiteListEditor.java
+++ org.jnegre.allthenews/src/org/jnegre/allthenews/pref/SiteListEditor.java
@@ -2,12 +2,18 @@ package org.jnegre.allthenews.pref;
 
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.preference.ListEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.jnegre.allthenews.Plugin;
+import org.jnegre.allthenews.search.SearchDialog;
 
 public class SiteListEditor extends ListEditor {
 
-    //protected Button editButton;
+    protected Button searchButton;
     
     /**
      * Constructor for SiteListEditor
@@ -68,37 +74,35 @@ public class SiteListEditor extends ListEditor {
 
 
     /**
+     * Adds the search button to usual buttons
      * @see org.eclipse.jface.preference.ListEditor#getButtonBoxControl(Composite)
      */
-//    public Composite getButtonBoxControl(Composite parent) {
-//        Composite composite = super.getButtonBoxControl(parent);
-//        editButton = new Button(composite,SWT.CENTER);
-//        editButton.setText("&Edit");
-//        //Copy the layout data...
-//        GridData gridData = new GridData();
-//        GridData originalGridData = (GridData)composite.getChildren()[0].getLayoutData();
-//        gridData.grabExcessHorizontalSpace = originalGridData.grabExcessHorizontalSpace;
-//        gridData.grabExcessVerticalSpace = originalGridData.grabExcessVerticalSpace;
-//        gridData.heightHint = originalGridData.heightHint;
-//        gridData.horizontalAlignment = originalGridData.horizontalAlignment;
-//        gridData.horizontalIndent = originalGridData.horizontalIndent;
-//        gridData.horizontalSpan = originalGridData.horizontalSpan;
-//        gridData.verticalAlignment = originalGridData.verticalAlignment;
-//        gridData.verticalSpan = originalGridData.verticalSpan;
-//        gridData.widthHint = originalGridData.widthHint;
-//        editButton.setLayoutData(gridData);
-//        editButton.addSelectionListener(new SelectionAdapter(){
-//            /**
-//             * @see org.eclipse.swt.events.SelectionAdapter#widgetSelected(SelectionEvent)
-//             */
-//            public void widgetSelected(SelectionEvent e) {
-//                super.widgetSelected(e);
-//                /**@todo... */
-//            }
-//
-//        });
-//        return composite;
-//    }
+    public Composite getButtonBoxControl(Composite parent) {
+        Composite composite = super.getButtonBoxControl(parent);
+        searchButton = new Button(composite,SWT.CENTER);
+        searchButton.setText("&Search...");
+        //Copy the layout data...
+        GridData gridData = new GridData();
+        GridData originalGridData = (GridData)composite.getChildren()[0].getLayoutData();
+        gridData.grabExcessHorizontalSpace = originalGridData.grabExcessHorizontalSpace;
+        gridData.grabExcessVerticalSpace = originalGridData.grabExcessVerticalSpace;
+        gridData.heightHint = originalGridData.heightHint;
+        gridData.horizontalAlignment = originalGridData.horizontalAlignment;
+        gridData.horizontalIndent = originalGridData.horizontalIndent;
+        gridData.horizontalSpan = originalGridData.horizontalSpan;
+        gridData.verticalAlignment = originalGridData.verticalAlignment;
+        gridData.verticalSpan = originalGridData.verticalSpan;
+        gridData.widthHint = originalGridData.widthHint;
+        searchButton.setLayoutData(gridData);
+        searchButton.addSelectionListener(new SelectionAdapter(){
+            public void widgetSelected(SelectionEvent e) {
+                SearchDialog sd = new SearchDialog(SiteListEditor.this.getShell());
+                sd.open();
+                //TODO use the result...
+            }
+        });
+        return composite;
+    }
 
 }
 
