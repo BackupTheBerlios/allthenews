2a81d59bf252e8b36de54f5ae0681f44ff5105b5
jnegre <jnegre>
2004-05-18 19:05:42 +0000

    code clean-up

--- org.jnegre.allthenews/src/org/jnegre/allthenews/Perspective.java
+++ org.jnegre.allthenews/src/org/jnegre/allthenews/Perspective.java
@@ -1,39 +1,29 @@
 package org.jnegre.allthenews;
-
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 //import org.eclipse.update.internal.ui.UpdatePerspective;
-
 /**
  * @author Laurent Fourrier, Jérôme Nègre
- *
+ *  
  */
 public class Perspective implements IPerspectiveFactory {
-
-/**
-* @see org.eclipse.ui.IPerspectiveFactory#createInitialLayout(IPageLayout)
-*/
-public void createInitialLayout(IPageLayout layout) {
-        String editorArea = layout.getEditorArea();
-        layout.setEditorAreaVisible(false);
-      
-		IFolderLayout bottom =
-		    layout.createFolder("bottom", IPageLayout.BOTTOM, 0.70f, editorArea);
+	/**
+	 * @see org.eclipse.ui.IPerspectiveFactory#createInitialLayout(IPageLayout)
+	 */
+	public void createInitialLayout(IPageLayout layout) {
+		String editorArea = layout.getEditorArea();
+		layout.setEditorAreaVisible(false);
+		IFolderLayout bottom = layout.createFolder("bottom",
+				IPageLayout.BOTTOM, 0.70f, editorArea);
 		bottom.addView("org.jnegre.allthenews.view.headline");
-		
-        IFolderLayout left =
-                layout.createFolder("left", IPageLayout.LEFT, 0.30f, editorArea);
-        left.addView("org.jnegre.allthenews.view.explorer");
-        IFolderLayout right =
-            layout.createFolder("right", IPageLayout.RIGHT, 0.70f,editorArea);
-        right.addView("org.jnegre.allthenews.view");
-        right.addView("org.jnegre.allthenews.view.browser");
-        //FIXME should use the new browser control
-        /*
-        IFolderLayout right =
-                layout.createFolder("right", IPageLayout.RIGHT, (float) 0.80,editorArea);
-        right.addView(UpdatePerspective.ID_BROWSER);
-        */
-}
+		IFolderLayout left = layout.createFolder("left", IPageLayout.LEFT,
+				0.30f, editorArea);
+		left.addView("org.jnegre.allthenews.view.explorer");
+		IFolderLayout right = layout.createFolder("right", IPageLayout.RIGHT,
+				0.70f, editorArea);
+		//FIXME remove this deprecated view
+		right.addView("org.jnegre.allthenews.view");
+		right.addView("org.jnegre.allthenews.view.browser");
+	}
 }
